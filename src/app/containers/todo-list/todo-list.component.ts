fcfd8b513f435d53f305d0c32a1a75453c3c0856
Luis Pérez <luis.perez@geekdacity.com>
2019-05-21 10:16:49 -0600

    Comment code

--- src/app/containers/todo-list/todo-list.component.ts
+++ src/app/containers/todo-list/todo-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from '../../models/todo.interface';
+import { Observable } from 'rxjs';
 import { Select, Store } from '@ngxs/store';
 import { TodoState } from '../../state/todo.state';
+import { Todo } from '../../models/todo.interface';
 import { RemoveTodo, ToggleTodo, ModifyTodo } from '../../actions/todo.actions';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -33,14 +33,29 @@ export class TodoListComponent implements OnInit {
     });
   }
 
+  /**
+   * Dispatchs action to delete a todo
+   *
+   * @param id - the id of the todo
+   */
   onDelete(id: number) {
     this.store.dispatch(new RemoveTodo(id));
   }
 
-  onToggle(data: { id: number, completed: boolean }) {
-    this.store.dispatch(new ToggleTodo({ id: data.id, completed: data.completed }));
+  /**
+   * Receives the toggle event emitted and dispatchs action to toggle todo (completed/incompleted)
+   *
+   * @param payload - todo data from event emitted
+   */
+  onToggle(payload: { id: number, completed: boolean }) {
+    this.store.dispatch(new ToggleTodo({ id: payload.id, completed: payload.completed }));
   }
 
+  /**
+   * Receives the edit event emitted and dispatches action to edit todo with new title
+   *
+   * @param payload - todo data from event emitted
+   */
   onEdit(payload: { id: number, title: string }) {
     this.store.dispatch(new ModifyTodo({ id: payload.id, title: payload.title }));
   }

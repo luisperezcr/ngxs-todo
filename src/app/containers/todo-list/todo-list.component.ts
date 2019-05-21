bca5bb7101548bc8c9aa6b18a4a202441098ca9e
Luis Pérez <luis.perez@geekdacity.com>
2019-05-20 22:04:18 -0600

    Get todo list

--- src/app/containers/todo-list/todo-list.component.ts
+++ src/app/containers/todo-list/todo-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { TodoState } from '../../state/todo.state';
+import { Observable } from 'rxjs';
+import { Todo } from '../../models/todo.interface';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent {
+  @Select(TodoState.getTodos)
+  todos$: Observable<Todo>;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }

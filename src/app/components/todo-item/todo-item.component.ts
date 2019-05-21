ff2e0ae72d024011a93eaa1954a9cce7b81e0d1d
Luis Pérez <luis.perez@geekdacity.com>
2019-05-21 10:22:34 -0600

    Clean  & comment code

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -23,22 +23,33 @@ export class TodoItemComponent {
 
   constructor() { }
 
+  // Emits event on delete click
   onDelete() {
     this.delete.emit(this.todo.id);
   }
 
+  // Toggles editing functionality
   onEditToggle() {
     this.isEditing = true;
   }
 
+  /**
+   * Emits event when the checkbox checked property is changed
+   */
   onChange() {
     this.toggle.emit({ id: this.todo.id, completed: !this.todo.completed });
   }
 
+  // Cancels editing (hides edit component)
   onCancel() {
     this.isEditing = false;
   }
 
+  /**
+   * Emits event with new todo data
+   *
+   * @param payload - todo data from event
+   */
   onEdit(payload: { id: number, title: string }) {
     this.edit.emit({ id: payload.id, title: payload.title });
     this.isEditing = false;

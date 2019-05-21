daab9750c344b1fafe9c2aa9b5d86c81cedd43d9
Luis Pérez <luis.perez@geekdacity.com>
2019-05-21 09:04:26 -0600

    Create add functionality

--- src/app/containers/todo-add/todo-add.component.ts
+++ src/app/containers/todo-add/todo-add.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { AddTodo } from '../../actions/todo.actions';
+import { Todo } from 'src/app/models/todo.interface';
 
 @Component({
   selector: 'app-todo-add',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-add.component.scss']
 })
 export class TodoAddComponent implements OnInit {
+  @ViewChild('todo') todo: ElementRef;
 
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit() {
   }
 
+  onBlur() {
+    const title = this.todo.nativeElement.value;
+    if (title) {
+      const newTodo: Todo = {
+        id: +new Date(),
+        title: title,
+        completed: false,
+        editing: false
+      };
+      this.store.dispatch(new AddTodo(newTodo)).toPromise().then(() => {
+        this.todo.nativeElement.value = '';
+      });
+    }
+  }
+
+  onKey(event: KeyboardEvent) {
+    const title = this.todo.nativeElement.value;
+    if (event.key === 'Enter' && title) {
+      const newTodo: Todo = {
+        id: +new Date(),
+        title: title,
+        completed: false,
+        editing: false
+      };
+      this.store.dispatch(new AddTodo(newTodo)).toPromise().then(() => {
+        this.todo.nativeElement.value = '';
+        this.todo.nativeElement.blur();
+      });
+    }
+  }
 }

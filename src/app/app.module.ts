c255ef2e23b2ae530218d98c6294ac62fcd1ce16
Luis Pérez <luis.perez@geekdacity.com>
2019-05-21 09:38:43 -0600

    Remove dev plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 import { NgxsModule } from '@ngxs/store';
 import { TodoState } from './state/todo.state';
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin'; 
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './containers/todo-list/todo-list.component';
@@ -29,9 +27,7 @@ import { TodoEditComponent } from './components/todo-edit/todo-edit.component';
     MaterialModule,
     NgxsModule.forRoot([
       TodoState
-    ]),
-    NgxsReduxDevtoolsPluginModule.forRoot(),
-    NgxsLoggerPluginModule.forRoot()
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

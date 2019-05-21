70acd6375a6274955e07bbd3f5dc35d289e96fad
Luis Pérez <luis.perez@geekdacity.com>
2019-05-21 00:13:20 -0600

    Implement delete functionality to item

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Todo } from '../../models/todo.interface';
 
 @Component({
@@ -6,13 +6,16 @@ import { Todo } from '../../models/todo.interface';
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.scss']
 })
-export class TodoItemComponent implements OnInit {
+export class TodoItemComponent {
   @Input()
   todo: Todo;
 
+  @Output()
+  delete: EventEmitter<number> = new EventEmitter<number>();
+
   constructor() { }
 
-  ngOnInit() {
+  onDelete() {
+    this.delete.emit(this.todo.id);
   }
-
 }
--- src/app/containers/todo-list/todo-list.component.ts
+++ src/app/containers/todo-list/todo-list.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
-import { Select } from '@ngxs/store';
-import { TodoState } from '../../state/todo.state';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../models/todo.interface';
+import { Select, Store } from '@ngxs/store';
+import { TodoState } from '../../state/todo.state';
+import { RemoveTodo } from '../../actions/todo.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
   @Select(TodoState.getTodos)
   todos$: Observable<Todo>;
 
-  constructor() { }
+  completedTodos: Todo[];
+  incompletedTodos: Todo[];
+
+  constructor(private store: Store) { }
+
+  ngOnInit() {
+    this.todos$.subscribe((todos: any) => {
+      this.completedTodos = [];
+      this.incompletedTodos = [];
+      todos.forEach((todo: Todo) => {
+        if (todo.completed) {
+          this.completedTodos.push(todo);
+        } else {
+          this.incompletedTodos.push(todo);
+        }
+      });
+    });
+  }
+
+  onDelete(id: number) {
+    this.store.dispatch(new RemoveTodo(id));
+  }
 }
